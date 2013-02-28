697e4cd6a4ccc6b9013970a130516427b63c063b
Kuzya <alexkuz@gmail.com>
2013-03-01 02:16:40 +0400

    fix linker related bug

--- ShareKit/libShareKit.linkwith.cs
+++ ShareKit/libShareKit.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libShareKit.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true, Frameworks = "Social Accounts AdSupport CFNetwork Twitter CoreLocation CoreGraphics MessageUI Security SystemConfiguration CoreFoundation", LinkerFlags = "-sqlite3.0")]
+[assembly: LinkWith ("libShareKit.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true, Frameworks = "Social Accounts AdSupport CFNetwork Twitter CoreLocation CoreGraphics MessageUI Security SystemConfiguration", LinkerFlags = "-lsqlite3.0")]
